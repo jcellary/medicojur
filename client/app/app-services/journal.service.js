56c04b52336769d335b3a4f65fb11a08edf20275
Jarosław Cellary <jcellary@gmail.com>
2015-12-23 17:08:17 +0100

    Journal Service in client

---
+++ client/app/app-services/journal.service.js
@@ -0,0 +1,54 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('JournalService', JournalService);
+
+    JournalService.$inject = ['$http', '$q'];
+    function JournalService($http, $q) {
+        var service = {};
+
+        service.GetAll = GetAll;
+        service.Create = Create;
+        service.Delete = Delete;
+
+        return service;
+
+        function GetAll() {
+            var journals = [{id:'1', filename:'file1', tags:'tag1, tag2'}, 
+                    {id:'2', filename:'file2', tags:'tag3, tag4'}];
+            
+            var deferred = $q.defer();
+            deferred.resolve(journals);
+            return deferred.promise;
+        
+            //return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
+        }
+
+        function Create(file, tags) {
+            var deferred = $q.defer();
+            deferred.resolve({ success: true });
+            return deferred.promise;
+        
+            //return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating journal'));
+        }
+        
+        function Delete(id) {
+            return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting journal'));
+        }
+
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -5,19 +5,25 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['UserService', '$rootScope'];
-    function HomeController(UserService, $rootScope) {
+    HomeController.$inject = ['$scope', 'UserService', 'JournalService', 'FlashService', '$rootScope'];
+    function HomeController($scope, UserService, JournalService, FlashService, $rootScope) {
         var vm = this;
 
+        $scope.file = null;
+        vm.fileReady = false;
         vm.user = null;
         vm.allUsers = [];
         vm.deleteUser = deleteUser;
+        vm.journals = [];
+        vm.deleteJournal = deleteJournal;
+        vm.upload = upload;
 
         initController();
 
         function initController() {
             loadCurrentUser();
             loadAllUsers();
+            loadJournals();
         }
 
         function loadCurrentUser() {
@@ -39,6 +45,41 @@
             .then(function () {
                 loadAllUsers();
             });
+        }
+
+        function loadJournals() {
+            JournalService.GetAll()
+                .then(function (journals) {
+                    vm.journals = journals;
+                });
+        }
+
+        function deleteJournal(id) {
+            JournalService.Delete(id)
+            .then(function () {
+                loadJournals();
+            });
+        }
+        
+        $scope.uploadedFile = function(element) {
+            $scope.$apply(function($scope) {
+                $scope.file = element.files[0];
+                vm.fileReady = true;           
+            });
+        }
+
+        function upload() {
+            vm.dataLoading = true;
+            JournalService.Create($scope.file, vm.uploadedJournal.tags)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Upload successful', true);
+                        vm.fileReady = false;
+                    } else {
+                        FlashService.Error(response.message);
+                    }
+                    vm.dataLoading = false;
+                });
         }
     }
 
